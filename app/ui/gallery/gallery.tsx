2ed5defa799523e1fb662b435f1c5c39062c5ce1
Wladimir Oliveira <wladimir4019@gmail.com>
2026-01-17 03:47:57 -0300

    add `images` and add `gallery`

--- app/ui/gallery/gallery.tsx
+++ app/ui/gallery/gallery.tsx
@@ -2,12 +2,23 @@
 
 import Image from "next/image";
 import thumbs from "@/app/ui/gallery/modules.json";
-import { CopyIcon } from "lucide-react";
+import { CopyIcon, XCircleIcon } from "lucide-react";
 import { useState } from "react";
 import clsx from "clsx";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 
 export function GalleryContainer() {
   const [category, setCategory] = useState("eventosDecoracoes");
+  const [open, setOpen] = useState(false);
+  const [type, setType] = useState(1);
   const current = thumbs.filter((thumb) => thumb?.group === category);
   return (
     <div className="flex-1 w-full p-4 rounded-lg bg-primaria max-w-4xl">
@@ -47,15 +58,72 @@ export function GalleryContainer() {
       </div>
       <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {current.map((thumb) => (
-          <li key={thumb?.id} className="">
-            <GalleryPhoto
-              src={thumb?.image}
-              text={thumb?.name}
-              count={thumb?.photo.length > 0 && thumb?.photo.length}
-            />
+          <li key={thumb?.id}>
+            <button
+              className="cursor-pointer"
+              onClick={() => {
+                setOpen(true);
+                setType(thumb?.id);
+              }}
+            >
+              <GalleryPhoto
+                src={thumb?.image}
+                text={thumb?.name}
+                count={thumb?.photo.length > 0 && thumb?.photo.length}
+              />
+            </button>
           </li>
         ))}
       </ul>
+      <div
+        className={clsx(
+          "flex flex-col fixed opacity-0 p-8 top-0 left-0 size-full backdrop-blur-sm transition-all bg-white/30",
+          {
+            "pointer-events-auto opacity-100": open,
+            "pointer-events-none opacity-0": !open,
+          },
+        )}
+      >
+        <div className="w-full max-w-[1280px] p-4 text-right">
+          <Button
+            className="cursor-pointer bg-white hover:bg-white/70"
+            size="icon"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <XCircleIcon className="text-black" />
+          </Button>
+        </div>
+        <div className="mx-auto">
+          <Carousel className="w-full max-w-2xs sm:max-w-md">
+            <CarouselContent>
+              {thumbs[type - 1]?.photo?.map((photo) => (
+                <CarouselItem key={photo?.id}>
+                  <div className="">
+                    <Card>
+                      <CardContent className="flex flex-1 flex-col aspect-square items-center justify-center p-6">
+                        <Image
+                          src={photo?.image}
+                          height={947}
+                          width={710}
+                          alt={photo?.name}
+                          className="rounded-sm"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                  {/* <div className="p-1">
+                    
+                  </div> */}
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="hidden sm:flex" />
+            <CarouselNext className="hidden sm:flex" />
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 }
@@ -70,16 +138,16 @@ export function GalleryPhoto({
   count?: number | false;
 }) {
   return (
-    <button className="border p-4 bg-white rounded-lg transition-all cursor-pointer hover:scale-[102%] hover:shadow-xl active:scale-100 active:shadow-xs">
+    <div className="border border-black/70 p-4 bg-white rounded-lg transition-all hover:scale-[102%] hover:shadow-xl active:scale-100 active:shadow-xs">
       <Image className="w-35 mb-2" src={src} height={246} width={193} alt="" />
-      <div className="flex justify-between">
+      <div className="flex items-center gap-1 justify-between">
         <p className="text-left max-w-30">{text}</p>
-        <p>
+        <p className="flex text-xs gap-1 items-center">
           {count}
-          {count && <CopyIcon />}
+          {count && <CopyIcon size="14" />}
         </p>
       </div>
-    </button>
+    </div>
   );
 }
 
@@ -98,9 +166,9 @@ export function CategoryButton({
     <button
       onClick={action}
       className={clsx(
-        "whitespace-nowrap p-4 mb-4 font-medium transition-all hover:border-b",
+        "whitespace-nowrap cursor-pointer p-4 mb-4 font-medium transition-all hover:border-b border-black/70",
         {
-          "border-b": category === group,
+          "border-b border-black/70": category === group,
         },
       )}
     >
